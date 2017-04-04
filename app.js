0aeddb3f911128bf1d58ae1df8dcfc5dcfc9a8ab
Calvin Szeto <szeto.calvin@gmail.com>
2017-04-04 18:18:23 -0400

    Generalize project.

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-var request_options = {
-  url: 'https://www.pollen.com/api/forecast/current/pollen/10011',
-  headers: {
-    'Referer': 'https://www.pollen.com/forecast/current/pollen/10011'
-  }
-};
+app.get('/pollen', function(req, res) {
+  var request_options = {
+    url: 'https://www.pollen.com/api/forecast/current/pollen/10011',
+    headers: {
+      'Referer': 'https://www.pollen.com/forecast/current/pollen/10011'
+    }
+  };
 
-app.get('/', function(req, res) {
   request(request_options, function(error, response, body) {
     if (error) {
       console.log('Error with pollen request:', error);
